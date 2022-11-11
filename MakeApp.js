467fe73a7201fe780930c244bd7b93d75508253d
faraz412 <ahmedfaraz412@gmail.com>
2022-11-11 23:26:28 +0530

    completed day-4 tasks

--- MakeApp.js
+++ MakeApp.js
@@ -16,7 +16,7 @@ let futureFav=[
     "https://images-static.nykaa.com/uploads/b74ecc67-7679-46b5-8a99-93791d213f36.jpg",
     "https://images-static.nykaa.com/uploads/470b740a-37a1-4039-8aa7-6c6616ee8111.jpg",
     "https://images-static.nykaa.com/uploads/65bc3a78-1b69-4105-b830-ea9848244562.gif"
-]
+];
 
 let onlyLuxe=[
     "https://images-static.nykaa.com/uploads/d4f5355c-ee62-4f30-b262-bbdb27bbc297.jpg",
@@ -31,8 +31,7 @@ let onlyLuxe=[
     "https://images-static.nykaa.com/uploads/7525207b-da12-4b9a-8877-eccb299b451d.jpg",
     "https://images-static.nykaa.com/uploads/4bbcac86-26d2-4ea7-ba7b-d82e7510b800.jpg",
     "https://images-static.nykaa.com/uploads/31453344-8e42-4756-a1a6-fd2ea20fd830.jpg"
-]
-
+];
 let skincareSos=[
     "https://images-static.nykaa.com/uploads/a5521cb8-ff92-4500-a5ea-fa2c56426b9b.jpg",
     "https://images-static.nykaa.com/uploads/5342875c-0f50-4ece-a7ee-db606967559e.jpg",
@@ -40,13 +39,195 @@ let skincareSos=[
     "https://images-static.nykaa.com/uploads/399a5d6d-3fe6-431c-a9c6-b36358e7ca83.jpg",
     "https://images-static.nykaa.com/uploads/2db137b6-0f31-45e0-87e7-9324e5c5b8a9.jpg",
     "https://images-static.nykaa.com/uploads/0ae7a493-efc2-4a51-98f2-8bef95751717.jpg"
+];
+
+let lss=[
+    {
+        "title": "Caudalie Vinergetic Instant Detox Mask",
+        "avatar": "https://images-static.nykaa.com/media/catalog/product/tr:h-800,w-800,cm-pad_resize/c/8/c8af0743522931003525_1.jpg",
+        "price": "958.00",
+        "qty": "75 ml",
+        "rating": "★★★★☆",
+        "id": "1"
+    },
+    {
+        "title": "Bobbi Brown Skin Full Cover Concealer Mini",
+        "avatar": "https://images-static.nykaa.com/media/catalog/product/tr:h-800,w-800,cm-pad_resize/0/a/0a30390716170295640_0.jpg",
+        "price": "54.00",
+        "qty": "2 ml",
+        "rating": "★★★☆☆",
+        "id": "2"
+    },
+    {
+        "title": "Murad Rapid Dark Spot Correcting Serum",
+        "avatar": "https://images-static.nykaa.com/media/catalog/product/tr:h-800,w-800,cm-pad_resize/5/d/5df0988767332153636_1.jpg",
+        "price": "238.00",
+        "qty": "10 ml",
+        "rating": "★★★★☆",
+        "id": "3"
+    },
+    {
+        "title": "L'Occitane Immortelle Precious Emulsion",
+        "avatar": "https://images-static.nykaa.com/media/catalog/product/tr:h-800,w-800,cm-pad_resize/5/6/560d90327SE075I22.jpg",
+        "price": "974.00",
+        "qty": "100 ml",
+        "rating": "★★★★★",
+        "id": "4"
+    },
+    {
+        "title": "Gucci Bloom Eau De Toilette",
+        "avatar": "https://images-static.nykaa.com/media/catalog/product/tr:h-800,w-800,cm-pad_resize/b/a/ba776303616302514298_1.jpg",
+        "price": "262.00",
+        "qty": "90 ml",
+        "rating": "★★★★★",
+        "id": "5"
+    },
+    {
+        "title": "PIXI Glow Mist",
+        "avatar": "https://images-static.nykaa.com/media/catalog/product/tr:h-800,w-800,cm-pad_resize/i/m/image_1_30_79.jpg",
+        "price": "491.00",
+        "qty": "50 ml",
+        "rating": "★★★★☆",
+        "id": "6"
+    },
+    {
+        "title": "Estee Lauder Serum",
+        "avatar": "https://images-static.nykaa.com/media/catalog/product/tr:h-800,w-800,cm-pad_resize/c/a/ca7979aESTEE00000404.jpg",
+        "price": "80.00",
+        "qty": "5 ml",
+        "rating": "★★★★☆",
+        "id": "7"
+    },
+    {
+        "title": "Jo Malone London Peony",
+        "avatar": "https://images-static.nykaa.com/media/catalog/product/tr:h-800,w-800,cm-pad_resize/8/c/8c9c05c690251081516_0.jpg",
+        "price": "865.00",
+        "qty": "10 ml",
+        "rating": "★★★★★",
+        "id": "8"
+    },
+    {
+        "title": "Forest Essentials Artisanal",
+        "avatar": "https://images-static.nykaa.com/media/catalog/product/tr:h-800,w-800,cm-pad_resize/a/e/ae4c9f18905452155112_1.jpg",
+        "price": "98.00",
+        "qty": "50 ml",
+        "rating": "★★★☆☆",
+        "id": "9"
+    }
+];
+
+let kc=[{
+    "title": "Givenchy Mini Lerouge",
+    "avatar": "https://images-static.nykaa.com/media/catalog/product/tr:h-800,w-800,cm-pad_resize/0/3/03fba90GIVEN00000241_0.jpg",
+    "price": "572.00",
+    "qty": "75 ml",
+    "rating": "★★★★☆",
+    "id": "10"
+},
+{
+    "title": "Kama Ayurveda Skincare",
+    "avatar": "https://images-static.nykaa.com/media/catalog/product/tr:h-800,w-800,cm-pad_resize/1/0/1000x1000px_image1-3.jpg",
+    "price": "944.00",
+    "qty": "2 ml",
+    "rating": "★★★☆☆",
+    "id": "11"
+},
+{
+    "title": "Smashbox Lipstick Duo",
+    "avatar": "https://images-static.nykaa.com/media/catalog/product/tr:h-800,w-800,cm-pad_resize/2/0/20ba6aeSMASH00000104_0.jpg",
+    "price": "624.00",
+    "qty": "10 ml",
+    "rating": "★★★★☆",
+    "id": "12"
+},
+{
+    "title": "The Ordinary The No-Brainer Set",
+    "avatar": "https://images-static.nykaa.com/media/catalog/product/tr:h-800,w-800,cm-pad_resize/7/b/7b9aa36THECI00000008_PI.jpg",
+    "price": "377.00",
+    "qty": "100 ml",
+    "rating": "★★★★★",
+    "id": "13"
+},
+{
+    "title": "Clinique Hydrate & Glow Set",
+    "avatar": "https://images-static.nykaa.com/media/catalog/product/tr:h-800,w-800,cm-pad_resize/9/e/9e3d490CLINI00000214_1.jpg",
+    "price": "722.00",
+    "qty": "90 ml",
+    "rating": "★★★★★",
+    "id": "14"
+},
+{
+    "title": "Nudestix Mini Sunkissed Kit",
+    "avatar": "https://images-static.nykaa.com/media/catalog/product/tr:h-800,w-800,cm-pad_resize/7/0/701f9f7NUDES00000092_0.jpg",
+    "price": "143.00",
+    "qty": "50 ml",
+    "rating": "★★★★☆",
+    "id": "15"
+}
 ]
+
+let fmh=[{
+    "title": "Carolina Herrera",
+    "avatar": "https://images-static.nykaa.com/media/catalog/product/tr:h-800,w-800,cm-pad_resize/6/9/69fef908411061995754.jpg",
+    "price": "145.00",
+    "qty": "50 ml",
+    "rating": "★★★☆☆",
+    "id": "16"
+},
+{
+    "title": "Gucci Bloom Eau De Toilette",
+    "avatar": "https://images-static.nykaa.com/media/catalog/product/tr:h-800,w-800,cm-pad_resize/b/a/ba776303616302514298_1.jpg",
+    "price": "612.00",
+    "qty": "10 ml",
+    "rating": "★★★★★",
+    "id": "17"
+},
+{
+    "title": "Yves Saint Laurent",
+    "avatar": "https://images-static.nykaa.com/media/catalog/product/tr:h-800,w-800,cm-pad_resize/1/2/12d2e183614272648425_1.jpg",
+    "price": "878.00",
+    "qty": "5 ml",
+    "rating": "★★★★☆",
+    "id": "18"
+},
+{
+    "title": "DIOR J'adore Eau De Parfum",
+    "avatar": "https://images-static.nykaa.com/media/catalog/product/tr:h-800,w-800,cm-pad_resize/6/0/6033e6b3348900417885_a.jpg",
+    "price": "478.00",
+    "qty": "50 ml",
+    "rating": "★★★★☆",
+    "id": "19"
+},
+{
+    "title": "Versace Pour Femme",
+    "avatar": "https://images-static.nykaa.com/media/catalog/product/tr:h-800,w-800,cm-pad_resize/8/0/8011003839094_dylanblue-pf_30ml_bottle.jpg",
+    "price": "922.00",
+    "qty": "75 ml",
+    "rating": "★★★★☆",
+    "id": "20"
+},
+{
+    "title": "HERMES Terre D'hermes",
+    "avatar": "https://images-static.nykaa.com/media/catalog/product/tr:h-800,w-800,cm-pad_resize/e/c/ecfcbef3346130012269_1.jpg",
+    "price": "981.00",
+    "qty": "2 ml",
+    "rating": "★★★☆☆",
+    "id": "21"
+}
+];
+
+
+
+
 let l;
 offerFlash();
 autoSS();
-slideImg(futureFav,725,10,"slide1","#imgCont","#next","#prev");
-slideImg(onlyLuxe,350,20,"slide2","#imgCont2","#next2","#prev2");
-slideImg(skincareSos,280,20,"slide3","#imgCont3","#next3","#prev3");
+slideImg(futureFav,725,10,"slide1","#imgCont","#next","#prev",1);
+slideImg(onlyLuxe,350,20,"slide2","#imgCont2","#next2","#prev2",3);
+slideImg(skincareSos,280,20,"slide3","#imgCont3","#next3","#prev3",3);
+slideData(lss,250,20,"slidels","#dataContls","#nextls","#prevls",4);
+slideData(kc,250,20,"slidekc","#dataContkc","#nextkc","#prevkc",4);
+slideData(fmh,250,20,"slidefmh","#dataContfmh","#nextfmh","#prevfmh",4);
 
 function offerFlash(){
     let i=0;
@@ -85,7 +266,7 @@ function autoSS(){
 };
 
 
-function slideImg(arr,x,y,idname,imgcont,bn,bp){
+function slideImg(arr,x,y,idname,imgcont,bn,bp,stop){
     for(let i=0; i<=arr.length-1; i++){
         let a=document.createElement("img");
         a.setAttribute("id",idname);
@@ -100,7 +281,7 @@ function slideImg(arr,x,y,idname,imgcont,bn,bp){
     a.style.left=`${l}px`;
     document.querySelector(bn).addEventListener("click",function(){
         l=l-(z);
-        if(l<((-z)*(arr.length-1))){
+        if(l<((-z)*(arr.length-stop))){
              l=0;
          }
         console.log(l);
@@ -109,10 +290,63 @@ function slideImg(arr,x,y,idname,imgcont,bn,bp){
     document.querySelector(bp).addEventListener("click",function(){
         l=l+z;
         if(l>0){
-            l=(-z*(arr.length-1));
+            l=(-z*(arr.length-stop));
         }
         console.log(l);
         a.style.left=`${l}px`;
     })
 }
 
+function slideData(arr,x,y,idname,imgcont,bn,bp,stop){
+    arr.forEach((elem,index)=>{
+        let div=document.createElement("div");
+        div.setAttribute("id",idname);
+        div.setAttribute("width",`${x}px`);
+        div.setAttribute("margin-right",`${y}px`);
+        let pic=document.createElement("img");
+        pic.setAttribute("src",elem.avatar);
+        let title=document.createElement("h4")
+        title.innerText=elem.title;
+        let price=document.createElement("h3");
+        price.innerText=elem.price;
+        let rating=document.createElement("p");
+        rating.innerText=elem.rating;
+        let qty=document.createElement("p");
+        qty.innerText=elem.qty;
+
+        div.append(pic,title,price,rating,qty);
+        document.querySelector(imgcont).append(div);
+    })
+    let a=document.querySelector(imgcont);
+    let z=x+y;  
+    l=0;
+    a.style.left=`${l}px`;
+    document.querySelector(bn).addEventListener("click",function(){
+        l=l-(z);
+        if(l<((-z)*(arr.length-stop))){
+             l=0;
+         }
+        console.log(l);
+        a.style.left=`${l}px`;
+    })
+    document.querySelector(bp).addEventListener("click",function(){
+        l=l+z;
+        if(l>0){
+            l=(-z*(arr.length-stop));
+        }
+        console.log(l);
+        a.style.left=`${l}px`;
+    })
+}
+
+
+// let hov=document.querySelectorAll(".ddu");
+// let botnav=document.querySelector("#bottomNav");
+// for(let i=0; i<=hov.length-1; i++){
+//     hov[i].addEventListener("mouseover",function(){
+//         botnav.style.display="none";
+//     })
+// }
+
+
+   
